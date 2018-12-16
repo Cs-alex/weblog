dd9e6de8d9f63cf656f48a217e63e399aae722bb
Cs-alex <alex.csende@gmail.com>
2018-12-16 12:59:17 +0100

    yx

--- libs/Database.php
+++ libs/Database.php
@@ -3,7 +3,7 @@
 class Database extends PDO {
 
     public function __construct() {
-        parent::__construct('mysql:host=localhost;dbname=weblog;charset=utf8mb4', 'root', '');
+        parent::__construct('mysql:host=b14a62853801ce:b0fd6c40@eu-cdbr-west-02.cleardb.net;dbname=heroku_0236e2be96e9508;charset=utf8mb4', 'b14a62853801ce', 'b0fd6c40');
     }
 
 }
