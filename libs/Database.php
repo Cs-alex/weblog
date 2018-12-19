62a4769f42f4bb90f89029ed2c369a840bddb667
Cs-alex <alex.csende@gmail.com>
2018-12-19 16:29:30 +0100

    xy

--- libs/Database.php
+++ libs/Database.php
@@ -3,7 +3,7 @@
 class Database extends PDO {
 
     public function __construct() {
-        parent::__construct('mysql:host=b14a62853801ce:b0fd6c40@eu-cdbr-west-02.cleardb.net;dbname=heroku_0236e2be96e9508;charset=utf8mb4', 'b14a62853801ce', 'b0fd6c40');
+        parent::__construct('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_0236e2be96e9508;charset=utf8mb4', 'b14a62853801ce', 'b0fd6c40');
     }
 
 }
