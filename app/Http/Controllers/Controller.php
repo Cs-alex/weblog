c871ee614228c865cded463131a431781ea87cc0
Cs-alex <alex.csende@gmail.com>
2019-04-12 19:46:48 +0200

    teszt

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,6 +13,6 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function index() {
-        
+        return view('dashboard.index');
     }
 }
