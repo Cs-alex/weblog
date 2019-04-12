42d35ca83d5e337d4dd907e6e196ce284b04fcb3
Cs-alex <alex.csende@gmail.com>
2019-04-12 21:37:34 +0200

    t

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
         <title>WeBlog</title>
-        <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{asset('public/css/flags/flag-icon.css')}}">
-        <link rel="stylesheet" href="{{asset('public/css/fontawesome/css/all.css')}}">
-        <link rel="stylesheet" href="{{asset('public/css/__base.css')}}">
-        <link rel="icon" type="image/png" href="{{asset('public/img/smallicon.png')}}">
-        <script type="text/javascript" src="{{asset('public/js/jquery.js')}}"></script>
-        <script type="text/javascript" src="{{asset('public/js/__base.js')}}"></script>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/flags/flag-icon.css')}}">
+        <link rel="stylesheet" href="{{asset('css/fontawesome/css/all.css')}}">
+        <link rel="stylesheet" href="{{asset('css/__base.css')}}">
+        <link rel="icon" type="image/png" href="{{asset('img/smallicon.png')}}">
+        <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/__base.js')}}"></script>
         @yield('content')
         <script id="dsq-count-scr" src="//http-localhost-weblog.disqus.com/count.js" async></script>
     </body>
