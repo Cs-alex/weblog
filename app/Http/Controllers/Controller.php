3a2eace64d784f3b4d4d711e54f8a216ddbfb4f4
Cs-alex <alex.csende@gmail.com>
2019-04-13 19:38:43 +0200

    t

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function index() {
-		echo $_SERVER['HTTP_X_FORWARDED_FOR'].' + '.gethostbyaddr($_SERVER['REMOTE_ADDR']);
+		echo $_SERVER['HTTP_X_FORWARDED_FOR'].' + '.gethostbyaddr($_SERVER['HTTP_X_FORWARDED_FOR']);
         //return Redirect::to('https://csalex-weblog.herokuapp.com/hu');
     }
 }
