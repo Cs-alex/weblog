176f68b9b877b61c82fa56972a0afa95e5bf25ec
Cs-alex <alex.csende@gmail.com>
2019-04-13 19:31:33 +0200

    t

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Model
 
     public function setUser() {
         $token = md5($_SERVER['REMOTE_ADDR'].gethostbyaddr($_SERVER['REMOTE_ADDR']));
-		echo md5($_SERVER['REMOTE_ADDR'].' + '.gethostbyaddr($_SERVER['REMOTE_ADDR']));
+		echo md5($_SERVER['HTTP_X_FORWARDED_FOR'].' + '.gethostbyaddr($_SERVER['REMOTE_ADDR']));
         $visitor = $this->select('id')->where('token', $token)->first();
         if ($visitor == FALSE) {
             $this->insertGetId(['token' => $token]);
