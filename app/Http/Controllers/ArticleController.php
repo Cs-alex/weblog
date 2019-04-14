b5ed9d5e4d2b353aaf40893d9814f3668b23c8fa
Cs-alex <alex.csende@gmail.com>
2019-04-14 13:13:03 +0200

    t

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,18 +20,16 @@ class ArticleController extends Controller
             session(['lang' => 'hu']);
         }
         App()->setlocale(session('lang'));
-        $article_id = $this->getArticleId(session('lang'), $article);
+        $article_id = $this->getArticleId(session('lang'), $article)->id;
         Session::put('scheme', DB::table('visitors')->select('color_scheme')->where('id', $user_id)->first()->color_scheme);
         if ($article_id) {
-            echo $article_id;die;
             $data = array(
-                'article' => Article::where('id', $article_id->id)->first(),
-                'vote' => DB::table('article__vote')->select(DB::raw('count(upvote) as upvote, count(downvote) as downvote'))->where([['visitor_id', '=', $user_id->id], ['article_id', '=', $article_id->id]])->first(),
-                'upvote' => DB::table('article__vote')->select(DB::raw('count(upvote) as upvote'))->where('article_id', $article_id->id)->first(),
-                'downvote' => DB::table('article__vote')->select(DB::raw('count(downvote) as downvote'))->where('article_id', $article_id->id)->first(),
-                'visitor' => DB::table('article__visitor')->select(DB::raw('count(visitor_id) as visitor'))->where('article_id', $article_id->id)->first()
+                'article' => Article::where('id', $article_id)->first(),
+                'vote' => DB::table('article__vote')->select(DB::raw('count(upvote) as upvote, count(downvote) as downvote'))->where([['visitor_id', '=', $user_id], ['article_id', '=', $article_id]])->first(),
+                'upvote' => DB::table('article__vote')->select(DB::raw('count(upvote) as upvote'))->where('article_id', $article_id)->first(),
+                'downvote' => DB::table('article__vote')->select(DB::raw('count(downvote) as downvote'))->where('article_id', $article_id)->first(),
+                'visitor' => DB::table('article__visitor')->select(DB::raw('count(visitor_id) as visitor'))->where('article_id', $article_id)->first()
             );
-            print_r($data);die;
             return view('articles.index')->with('data', $data);
         } else {
             return view('errors.404');
